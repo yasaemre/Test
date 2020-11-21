65ef9083c666e62c6b36954fc64b771a9f3dfc44
Emre Yasa <emreyasa86@gmail.com>
2020-11-21 10:00:35 -0800

    Chmoddfskjdf

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("hello emrekldjsdkjsdcfsddsff ds")
+        Text("hello emrekldjsdkjsdcdfsddsff ds")
             .padding()
     }
 }
