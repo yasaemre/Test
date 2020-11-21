5ae38bc21f3900b0bf5a01d6a27f4c59927567e0
yasaemre <eyasa@mail.ccsf.edu>
2020-11-21 08:29:32 -0800

    Initial Commit

---
+++ Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by Emre Yasa on 11/21/20.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
